7634d16254a5df92a021089c2e949a4afae40850
vpnvipin7 <vpnvipin7@users.noreply.github.com>
2019-01-06 19:34:28 +0100

    done

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -8,7 +8,7 @@ import { Observable } from 'rxjs/Observable';
 export class ProductService {
   private _albumUrl='../assets/album.json';
   constructor(private  _http:Http ) {}
-  getAlbum(id:number): Observable <Album>{
+  getAlbum(id:number): Observable <Album> {
     return this._http.get(this._albumUrl).map((response) => <Album> response.json());
   }
 
